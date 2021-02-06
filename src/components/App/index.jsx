bc8e61e9880f124b8e71fe89a5a0deeb84765199
uvg <nis16104@uvg.edu.gt>
2021-02-06 11:17:12 -0600

    first version main page

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from '../Header';
 import Main from '../Main';
-//import styles from './app.module.css';
+import styles from './app.module.css';
 
 const App = () => (
-  <>
+  <div className={styles.app}>
     <Header />
     
     <Main />
     {/** <Profile> */}
-  </>
+  </div>
 );
 
 export default App;
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from './main.module.css';
 
+const Post = () => (
+  <div className={styles.post}>
+  </div>
+)
+
 const Main = () => (
   <div className={styles.main}>
-    Main
+    <div className={styles.wall}>
+      {
+        [0,0,0,0,0].map((post) => (<Post />))
+      }
+    </div>
   </div>
 );
 
