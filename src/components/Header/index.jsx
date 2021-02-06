be9740e34bb57ada78a19b6fc82585827b2689ef
uvg <nis16104@uvg.edu.gt>
2021-02-06 12:22:03 -0600

    profile first steps

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -70,4 +70,4 @@ export default compose(
       }
     }),
   )
-)(Header)
+)(Header);
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import Wall from '../General/Wall';
+import { connect } from "react-redux";
 import styles from './main.module.css';
 
-const Post = () => (
-  <div className={styles.post}>
-  </div>
-)
-
-const Main = () => (
+const Main = ({
+  posts
+}) => (
   <div className={styles.main}>
-    <div className={styles.wall}>
-      {
-        [0,0,0,0,0].map((post) => (<Post />))
-      }
-    </div>
+    <Wall posts={posts}/>
   </div>
 );
 
-export default Main;
+export default connect(
+  (state) => ({
+    posts: [0,0,0,0,0], 
+  }),
+  (dispatch) => ({
+    fetchAllPost(values) {
+      console.log("fetch all post", values)
+    },
+    commentPost(values){
+      console.log('create comment', values)
+    },
+    createPost(values) {
+      console.log('create post', values)
+    }
+  }),
+)(Main);
