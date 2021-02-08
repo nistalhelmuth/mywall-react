e20871bd8a670aaa64d9a6e85a83c1863ffa0594
uvg <nis16104@uvg.edu.gt>
2021-02-08 11:30:37 -0600

    Wall comments and posts

--- src/components/General/Wall/index.jsx
+++ src/components/General/Wall/index.jsx
@@ -1,51 +1,29 @@
 import React from 'react';
+import Post from '../Post'
 import styles from './wall.module.css';
-
-const Comment = () => (
-  <div className={styles.comment}>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed bibendum purus. In hac habitasse platea dictumst. Cras id nulla tortor. Nam vestibulum vitae dolor in consectetur.
-    </p>
-    <p>
-      20/01/1233
-    </p>
-  </div>
-);
-
-const Post = () => (
-  <div className={styles.post}>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed bibendum purus. In hac habitasse platea dictumst. Cras id nulla tortor. Nam vestibulum vitae dolor in consectetur.
-    </p>
-    <p>
-      20/01/1233
-    </p>
-    <div className={styles.footer}>
-      <p>
-        comment
-      </p>
-      <p>
-        view
-      </p>
-    </div>
-    {
-      true && (
-        <div className={styles.bot}>
-          {
-            [0,0,0].map((post) => (<Comment />))
-          }
-        </div>
-      )
-    }
-  </div>
-)
 
 const Main = ({
   posts
 }) => (
   <div className={styles.wall}>
     {
-      posts.map((post) => (<Post />))
+      posts ? (
+        <>
+          {
+            posts.map((post) => (
+              <Post
+                content={post.content}
+                dateCreated={post.dateCreated}
+                comments={post.comments}
+              />
+            ))
+          }
+        </>
+      ) : (
+        <p>
+          No posts to display
+        </p>
+      )
     }
   </div>
 );
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Wall from '../General/Wall';
 import { connect } from "react-redux";
+import * as postActions from '../../actions/post';
+import * as selectors from '../../reducers';
 import styles from './main.module.css';
 
-const Main = ({
-  posts
-}) => (
-  <div className={styles.main}>
-    <Wall posts={posts}/>
-  </div>
-);
+class Main extends Component {
+  componentDidMount() {
+    const {
+      fetchAllPost
+    } = this.props;
+    fetchAllPost();
+  }
+
+  render() {
+    const {
+      posts
+    } = this.props;
+    return(
+      <div className={styles.main}>
+        <Wall posts={posts}/>
+      </div>
+    );
+  }
+
+}
 
 export default connect(
   (state) => ({
-    posts: [0,0,0,0,0], 
+    posts: selectors.getAllPosts(state),
   }),
   (dispatch) => ({
-    fetchAllPost(values) {
-      console.log("fetch all post", values)
+    fetchAllPost() {
+      dispatch(postActions.fetchAllPosts())
     },
     commentPost(values){
       console.log('create comment', values)
