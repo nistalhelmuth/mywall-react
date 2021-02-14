bfca36576584200d32cc7f9e6731613f93cf7bc5
uvg <nis16104@uvg.edu.gt>
2021-02-13 18:02:13 -0600

    redux persists

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,23 +1,31 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
-
 import reducer from './reducers';
 import mainSaga from './sagas';
 
 export const sagaMiddleware = createSagaMiddleware();
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [
+    'authReducer',
+  ],
+};
+
 const configureStore = () => {
+  const persistedReducer = persistReducer(persistConfig, reducer);
   const store = createStore(
-    reducer,
-    compose(
-      applyMiddleware(sagaMiddleware), 
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    )
+    persistedReducer,
+    applyMiddleware(sagaMiddleware),
   );
+  const persistor = persistStore(store);
 
   sagaMiddleware.run(mainSaga);
-
-  return store;
+  return { store, persistor };
 };
+
 
 export default configureStore;
